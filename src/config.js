4aa38b9a7c23f4f173b5ea1a543379b8130699bf
Bagus <bagus.nurhuda@ui.ac.id>
2022-06-15 22:16:17 +0700

    add

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:8080',
-  baseURL: 'https://kasbaik-v1.herokuapp.com/',
+  // baseURL: 'https://kasbaik-v1.herokuapp.com/',
+  baseURL: 'https://cc-app-vsnl2vtvja-et.a.run.app',
 });
 
 export default axiosInstance;
